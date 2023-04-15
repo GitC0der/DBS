94ef7567ad2ffa36f8cb4ff071f9b9c35a690c7d
Zhang-XC <zhang_xiaocheng@outlook.com>
2023-04-15 20:53:03 +0200

    E_1, E_2 and their outputs updated

--- G44/E_1.sql
+++ G44/E_1.sql
@@ -1,8 +1,8 @@
 SELECT DISTINCT B.business_name AS name, B.review_count
-FROM Business B, Business_location BL, Business_parking_type BPT, Parking_type PT,
-    Business_hours BH, Day_of_week DW, Reviews R
+FROM Business B, Business_location BL, Business_parking_type BPT, Parking_type PT, 
+     Day_of_week DW, Business_hours BH
 WHERE B.business_id = BL.business_id AND BL.city_name = 'las vegas'
-    AND B.business_id = BPT.business_id AND BPT.parking_type_id = PT.parking_type_id AND PT.parking_type_description = 'valet'
-    AND B.business_id = R.business_id AND R.stars = 5
-    AND B.business_id = BH.business_id AND BH.day_id = DW.day_id AND DW.day_name = 'Friday'
-ORDER BY B.business_name ASC;
+      AND B.business_id = BPT.business_id AND BPT.parking_type_id = PT.parking_type_id AND PT.parking_type_description = 'valet'
+      AND B.business_id = BH.business_id AND BH.day_id = DW.day_id AND DW.day_name = 'Friday'
+      AND B.stars = 5 
+ORDER BY B.business_name ASC
--- G44/E_2.sql
+++ G44/E_2.sql
@@ -1,10 +1,6 @@
-SELECT business_name, stars
-FROM (
-    SELECT DISTINCT B.business_name, R.stars
-    FROM Business B, Reviews R, Business_location BL
-    WHERE B.business_id = BL.business_id AND BL.state_name = 'CA'
-        AND B.business_id = R.business_id
-    ORDER BY R.stars DESC, B.business_name ASC
-)
+SELECT *
+FROM (SELECT DISTINCT B.business_name, B.stars
+      FROM Business B, Business_location BL
+      WHERE B.business_id = BL.business_id AND BL.state_name = 'CA'
+      ORDER BY B.stars DESC, B.business_name ASC)
 WHERE ROWNUM <= 10
-ORDER BY stars DESC, business_name ASC;
