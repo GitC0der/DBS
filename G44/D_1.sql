4746884d0d229184bbbd6a1ff28dd7989d2b937b
Danny <danny.seel@epfl.ch>
2023-04-17 08:37:54 +0200

    Approved changes

--- G44/D_1.sql
+++ G44/D_1.sql
@@ -1,9 +1,13 @@
-SELECT DISTINCT BL.city_name
-FROM Business_location BL
-WHERE BL.business_id IN (
+SELECT DISTINCT C.city_name 
+FROM Cities C
+WHERE C.city_name IN ( -- some Cities do not have businesses
+    SELECT DISTINCT BL.city_name
+    FROM Business_location BL
+) AND NOT EXISTS (
     SELECT BH.business_id
-    FROM Business_hours BH
+    FROM Business_location BL2, Business_hours BH
+    WHERE C.city_name = BL2.city_name AND BL2.business_id = BH.business_id
     GROUP BY BH.business_id
-    HAVING COUNT(DISTINCT BH.day_id) < 5
+    HAVING COUNT(DISTINCT BH.day_id) >= 5
 )
-ORDER BY BL.city_name ASC;
+ORDER BY C.city_name ASC
--- G44/D_4.sql
+++ G44/D_4.sql
@@ -1,7 +1,12 @@
-SELECT DISTINCT BL.city_name
-FROM Business_location BL, Reviews R
-WHERE BL.business_id = R.business_id
-GROUP BY BL.city_name, R.business_id
-HAVING COUNT(DISTINCT R.review_id) >= 2
-ORDER BY BL.city_name ASC
-FETCH FIRST 50 ROWS ONLY;
+SELECT DISTINCT C.city_name
+FROM Cities C
+WHERE C.city_name IN ( -- some Cities do not have businesses
+    SELECT DISTINCT BL.city_name
+    FROM Business_location BL
+) AND NOT EXISTS (
+    SELECT *
+    FROM Business_location BL2, Business B
+    WHERE C.city_name = BL2.city_name AND BL2.business_id = B.business_id AND B.review_count < 2
+)
+ORDER BY C.city_name
+FETCH FIRST 50 ROWS ONLY
