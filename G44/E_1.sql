84c4be00c2e630ee071883cc18da3a96b3e4c59c
Zhang-XC <zhang_xiaocheng@outlook.com>
2023-04-26 09:38:56 +0200

    E_1, E_10 and outputs updated

--- G44/E_1.sql
+++ G44/E_1.sql
@@ -1,7 +1,7 @@
 SELECT DISTINCT B.business_name AS name, B.review_count
 FROM Business B, Business_location BL, Business_parking_type BPT, Parking_type PT, 
      Day_of_week DW, Business_hours BH
-WHERE B.business_id = BL.business_id AND BL.city_name = 'las vegas'
+WHERE B.business_id = BL.business_id AND BL.city_name like '%las vegas%'
       AND B.business_id = BPT.business_id AND BPT.parking_type_id = PT.parking_type_id AND PT.parking_type_description = 'valet'
       AND B.business_id = BH.business_id AND BH.day_id = DW.day_id AND DW.day_name = 'Friday'
       AND B.stars = 5 
--- G44/E_10.sql
+++ G44/E_10.sql
@@ -1,7 +1,7 @@
 SELECT COUNT(DISTINCT BHC.business_id) AS count
 FROM Business_has_categories BHC, Business_categories BC, Business_hours BH,
     Day_of_week DW
-WHERE BHC.category_id = BC.category_id AND BC.category_name = 'Dry Cleaning'
+WHERE BHC.category_id = BC.category_id AND BC.category_name like '%Dry Cleaning%'
     AND BHC.business_id = BH.business_id AND
     ((BH.day_id = DW.day_id AND DW.day_name = 'Saturday'
         AND BH.hours_from IS NOT NULL AND BH.hours_to IS NOT NULL)
