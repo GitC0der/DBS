293129217fc5f01009fd7bbdc3555f43ad0a34a7
Zhang-XC <zhang_xiaocheng@outlook.com>
2023-04-26 09:00:48 +0200

    D_10 and outputs updated

--- G44/D_10.sql
+++ G44/D_10.sql
@@ -1,13 +1,11 @@
-SELECT SUM(U.review_count) as review_count, BL.city_name as city_name
-FROM Business_location BL, Business B, Reviews R, User_yelp U
-WHERE BL.business_id = B.business_id AND B.business_id = R.business_id AND R.user_id = U.user_id
-    AND U.user_id NOT IN (
-        SELECT F.user_id
-        FROM Friends F
-        WHERE F.user_id = U.user_id
-        GROUP BY F.user_id
-        HAVING COUNT(*) >= 3
-    )
+SELECT COUNT(R.review_id) AS review_count, BL.city_name
+FROM Business_location BL, Reviews R
+WHERE R.business_id = BL.business_id AND R.user_id IN (
+    SELECT F.user_id
+    FROM Friends F
+    GROUP BY F.user_id
+    HAVING COUNT(DISTINCT friends_id) < 3
+)
 GROUP BY BL.city_name
-ORDER BY review_count ASC, city_name ASC
-FETCH FIRST 1 ROW ONLY;
+ORDER BY review_count ASC, BL.city_name ASC
+FETCH FIRST 1 ROW ONLY
